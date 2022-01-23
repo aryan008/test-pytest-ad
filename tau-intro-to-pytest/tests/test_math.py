927ffe8f7c408670c73f16406f86d9799f72332a
aryan008 <adam.s.ryan@pwc.com>
2022-01-23 17:48:08 +0000

    Up to chapter 6

--- tau-intro-to-pytest/tests/test_math.py
+++ tau-intro-to-pytest/tests/test_math.py
@@ -1,3 +1,5 @@
+import pytest
+
 def test_one_plus_one():
     assert 1+1 ==2
 
@@ -6,3 +8,27 @@ def test_one_plus_two():
   b = 2
   c = 3
   assert a + b == c
+
+def test_divide_by_zero():
+  with pytest.raises(ZeroDivisionError) as e:
+    num = 1 / 0
+  
+  assert 'division by zero' in str(e.value)
+
+
+# --------------------------------------------------------------------------------
+# A parametrized test function
+# --------------------------------------------------------------------------------
+
+products = [
+  (2, 3, 6),            # postive integers
+  (1, 99, 99),          # identity
+  (0, 99, 0),           # zero
+  (3, -4, -12),         # positive by negative
+  (-5, -5, 25),         # negative by negative
+  (2.5, 6.7, 16.75)     # floats
+]
+
+@pytest.mark.parametrize('a, b, product', products)
+def test_multiplication(a, b, product):
+  assert a * b == product
